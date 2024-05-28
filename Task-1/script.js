98ff64b51bb66e6cc10afabb8d8103f198b9f630
Rahul Kumar <148069761+Rahulkumar2488@users.noreply.github.com>
2024-05-28 10:48:14 +0530

    Add files via upload

---
+++ Task-1/script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
